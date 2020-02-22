cfe693d86c34c3ae510e48b5ab9a30671f741ce7
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-22 19:10:48 +0100

    update FunctionalTable.lua

--- FunctionalTable.lua
+++ FunctionalTable.lua
@@ -1,14 +1,24 @@
-
-return function(arg)
-  local new = {}
-  local func = function() end
+ function n(arg)
+  local foregroundTable = {}
+  local func = function() print('hello world') end
   local desc = 'dummy function description'
   local protectMetatable = true
-  local allowNewIndex = true
-  setmetatable(new,{
-    __metatable = protectMetatable or nil,
-    __newindex = allowNewIndex and nil or false,
-    
-  })
-  return new
+  local disableNewIndex = true
+  local backgroundTable = {dummy = 1}
+  
+  assert('function'==type(func))
+  assert('string'==type(desc))
+  
+  local mt = {}
+  mt.__call = func
+  mt.__index = backgroundTable
+  mt.__metatable = protectMetatable and false or nil
+  mt.__newindex = disableNewIndex and function() error('Cant add new values' ,2) end or nil
+  mt.__tostring = function() return 'Functional Table: '..desc end
+  setmetatable(foregroundTable,mt)
+  return fore
 end
+
+local a = n()
+a()
+print(a.dummy)
