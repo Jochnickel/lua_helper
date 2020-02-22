35f0bc230a475f0b6892b4019ba9077b1e563513
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-22 18:54:35 +0100

    update FunctionalTable.lua

--- FunctionalTable.lua
+++ FunctionalTable.lua
@@ -1,1 +1,14 @@
 
+return function(arg)
+  local new = {}
+  local func = function() end
+  local desc = 'dummy function description'
+  local protectMetatable = true
+  local allowNewIndex = true
+  setmetatable(new,{
+    __metatable = protectMetatable or nil,
+    __newindex = allowNewIndex and nil or false,
+    
+  })
+  return new
+end
