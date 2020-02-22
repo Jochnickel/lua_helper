fedc28858b4e2c4ab7d867699449bff6209d28e1
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-22 20:15:48 +0100

    update FunctionalTable.lua

--- FunctionalTable.lua
+++ FunctionalTable.lua
@@ -5,15 +5,15 @@
 -- foo'hello world' -- prints hello world
 -- print(foo,foo.dummy1, foo.dummy2) -- prints 'functional table x1234; a print function', 1, 2
 
+local NO_DESC = 'no description'
 
-
-local function new(arg)
+local function new(arg,description)
   local argIsntTable = 'table'~=type(arg)
-  local foregroundTable = {}
+  local foregroundTable = argIsntTable and {} or arg.table or arg.tab or arg.t or {}
   local _tostr = string.format('%s',foregroundTable)
   local func = argIsntTable and arg or arg.function or arg.func or arg.f
-  local desc = argIsntTable and 'No description' or arg.description or arg.desc or arg.d
-  local protectMetatable = argIsntTable or not (arg.leakMetatable or arg.leak or arg.l)
+  local desc = argIsntTable and description or arg.description or arg.desc or arg.d or NO_DESC
+  local leakMetatable = not (argIsntTable or not (arg.leakMetatable or arg.leak or arg.l))
   local readOnly = argIsntTable or not (arg.writeable or arg.writable or arg.write or arg.w)
   local backgroundTable = {}
   
@@ -23,7 +23,7 @@ local function new(arg)
   local mt = {}
   mt.__call = func
   mt.__index = backgroundTable
-  mt.__metatable = protectMetatable and false or nil
+  mt.__metatable = leakMetatable and nil
   mt.__newindex = readOnly and function() error('Cant add new values' ,2) end or nil
   mt.__tostring = function()
     return string.format('functional %s; %s', _tostr, desc)
