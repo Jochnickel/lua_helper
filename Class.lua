74e07be6e9a4838119a937933e3f2deb2f519c3b
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-23 13:30:46 +0100

    update Class.lua

--- Class.lua
+++ Class.lua
@@ -1,5 +1,20 @@
 -- example usage and defaults
--- 
+-- local C1 = Class(function(self)
+--   local dummy1 = dummy1
+--   self.GetVal1 = function(self)
+--     return dummy1
+--   end
+--   self.SetVal1 = function(self,v)
+--     dummy1 = v
+--   end
+-- end)
+
+-- local C2 = Class{
+--   constructor = function(self,a)
+--     self.a = a
+--   end,
+--   self = {}
+-- }
 
 
 local new = function(arg)
