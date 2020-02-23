b319b48dccd1701434262f4ef2de8ea1e83b9d26
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-23 12:46:06 +0100

    update Class.lua

--- Class.lua
+++ Class.lua
@@ -1,1 +1,13 @@
+-- example usage and defaults
+-- 
 
+
+local new = function(arg)
+  local argIsntTable = 'table'==type(arg)
+  local constr = argIsntTable and arg or arg.constructor or arg.constr or arg.const or arg.const
+  local hiddenTable = {}
+  
+  
+end
+
+return new
